a6c4556daf302ab1fcfa98f35c96bd97aca0e48c
chenzepei3 <chenzepei3@xiaomi.com>
2026-08-24 21:16:06 +0800

    feat(bk7258): add Wi-Fi hardware driver shim

---
+++ chips/bk7258/wifi/glue/include/driver/aon_rtc.h
@@ -0,0 +1,31 @@
+/*
+ * chips/bk7258/wifi/glue/include/driver/aon_rtc.h
+ *
+ * NuttX reimplementation of the Armino AON-RTC driver surface used by the
+ * vendored glue. Skeleton: init/deinit are no-op success; period-timer create/
+ * destroy return BK_ERR_NOT_SUPPORT until the team aon-rtc driver exists.
+ */
+
+#ifndef __BK7258_WIFI_GLUE_DRIVER_AON_RTC_H
+#define __BK7258_WIFI_GLUE_DRIVER_AON_RTC_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <common/bk_err.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef uint32_t aon_rtc_id_t;
+typedef uint32_t rtc_tick_t;
+
+bk_err_t bk_aon_rtc_driver_init(void);
+bk_err_t bk_aon_rtc_driver_deinit(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __BK7258_WIFI_GLUE_DRIVER_AON_RTC_H */
